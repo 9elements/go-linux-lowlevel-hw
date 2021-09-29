a1b0104102d79fd286204e9db967e5a9718cc38f
Christopher Meis <christopher.meis@9elements.com>
2021-09-29 13:20:27 +0200

    Reduce API: * Adapt ReadMSR for universal usage * Remove ReadMSRAllCores * Adapt tests and related code
    Signed-off-by: Christopher Meis <christopher.meis@9elements.com>

--- pkg/hwapi/msr.go
+++ pkg/hwapi/msr.go
@@ -1,15 +1,12 @@
 package hwapi
 
 import (
-	"fmt"
-	"runtime"
-
 	"github.com/fearful-symmetry/gomsr"
 )
 
 // ReadMSR returns the MSR on core #0
-func (h HwAPI) ReadMSR(msr int64) (uint64, error) {
-	msrCtx, err := gomsr.MSR(0)
+func (h HwAPI) ReadMSR(msr int64, core int) (uint64, error) {
+	msrCtx, err := gomsr.MSR(core)
 	if err != nil {
 		return 0, err
 	}
@@ -19,26 +16,4 @@ func (h HwAPI) ReadMSR(msr int64) (uint64, error) {
 	}
 
 	return msrData, nil
-}
-
-// ReadMSRAllCores returns the MSR and errors if it's not the same on all cores
-func (h HwAPI) ReadMSRAllCores(msr int64) (uint64, error) {
-	var data uint64
-	for i := 0; i < runtime.NumCPU(); i++ {
-		msrCtx, err := gomsr.MSR(i)
-		if err != nil {
-			return 0, err
-		}
-		msrData, err := msrCtx.Read(msr)
-		if err != nil {
-			return 0, err
-		}
-		if i != 0 {
-			if data != msrData {
-				return 0, fmt.Errorf("MSR: cores of MSR 0x%x non equal", msr)
-			}
-		}
-		data = msrData
-	}
-	return data, nil
 }

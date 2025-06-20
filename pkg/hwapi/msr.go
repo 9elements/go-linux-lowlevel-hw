fcd4438026beb6b3d353aa71614b9c11eb9c76a6
Christian Walter <christian.walter@9elements.com>
2025-06-20 14:35:43 +0200

    fix: remove debug print
    Signed-off-by: Christian Walter <christian.walter@9elements.com>

--- pkg/hwapi/msr.go
+++ pkg/hwapi/msr.go
@@ -12,7 +12,6 @@ func (h HwAPI) ReadMSR(msr int64) []uint64 {
 	count := 0
 	var ret []uint64
 	for {
-		fmt.Printf("ReadMSR - gomsr.MSR count: %d\n", count)
 		msrCtx, err := gomsr.MSR(count)
 		if err != nil {
 			fmt.Fprintf(os.Stdout, "ReadMSR - gomsr.MSR context aborted with: %v\n", err)

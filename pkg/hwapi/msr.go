6070168f2b7b41c1fe64765a2a96b50c2465d855
Christian Walter <christian.walter@9elements.com>
2025-06-20 11:37:22 +0200

    fix: do not endless loop within ReadMSR
    The current implementation just endlessly loops within ReadMSR until we
    are running out of file descriptors. The commit _wanted_ to implement to
    read the MSR from all cores - the previous commit however implement to
    read core 0 until we read file_max.
    
    Signed-off-by: Christian Walter <christian.walter@9elements.com>

--- pkg/hwapi/msr.go
+++ pkg/hwapi/msr.go
@@ -12,6 +12,7 @@ func (h HwAPI) ReadMSR(msr int64) []uint64 {
 	count := 0
 	var ret []uint64
 	for {
+		fmt.Printf("ReadMSR - gomsr.MSR count: %d\n", count)
 		msrCtx, err := gomsr.MSR(count)
 		if err != nil {
 			fmt.Fprintf(os.Stdout, "ReadMSR - gomsr.MSR context aborted with: %v\n", err)
@@ -23,7 +24,9 @@ func (h HwAPI) ReadMSR(msr int64) []uint64 {
 			break
 		}
 		ret = append(ret, msrData)
+		count++
 	}
 
+	fmt.Printf("ReadMSR - gomsr.MSR count: %d, ret len: %d\n", count, len(ret))
 	return ret
 }
